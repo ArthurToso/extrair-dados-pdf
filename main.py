4aed2d8308a33e6f5a0546d17dc872d622e00eb3
ArthurToso <arthurtoso2003@gmail.com>
2025-10-18 22:28:10 -0300

    feat(dados_pdf): Terminei extração dos dados do PDF e Criação das pastas por data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import pdfplumber
 import re
+import shutil
 
 def ler_dados_pdf (dir_pdf):
     with pdfplumber.open(dir_pdf) as pdf:
@@ -11,6 +12,7 @@ def ler_dados_pdf (dir_pdf):
         nome_cliente_regex = r"nome\s+do\s+cliente:\s+(.*)"
         data_fatura_regex = r"data\s+da\s+fatura:\s+(.*)"
         valor_total_regex = r"valor\s+total:\s+(.*)"
+        detalhes_regex = r"detalhes:(.*)----------------------------------------"
     
         #Match id_cliente
         match = re.search(id_cliente_regex, texto_pdf)
@@ -36,6 +38,14 @@ def ler_dados_pdf (dir_pdf):
             dados_pdf["valor_total"] = match.group(1)
         else:
             dados_pdf["valor_total"] = "Valor Total Não Encontrado"
+        #Match detalhes
+        match = re.search(detalhes_regex, texto_pdf, re.DOTALL)
+        if match:
+            texto_aux = match.group(1).strip()
+            detalhes_list = texto_aux.split("\n")
+            dados_pdf["detalhes"] = detalhes_list
+        else:
+            dados_pdf["detalhes"] = "Detalhes Não Encontrado"
     return dados_pdf
 
 dir_pdf = os.path.dirname(__file__)
@@ -43,6 +53,15 @@ dir_pdf = os.path.dirname(__file__)
 pdf_list = [f for f in os.listdir(dir_pdf) if f.endswith(".pdf") or f.endswith(".PDF")]
 
 for arquivo in pdf_list:
-    print(arquivo)
-    dados_pdf = ler_dados_pdf(arquivo)
-    print(dados_pdf)
+    try:
+        dados_pdf = ler_dados_pdf(arquivo)
+        print(dados_pdf.get("detalhes"))
+        nome_pasta = str(dados_pdf.get("data_fatura"))
+        nome_pasta = nome_pasta.replace("/","-")
+        dir_pasta = os.path.join(dir_pdf, nome_pasta)
+        if not os.path.exists(dir_pasta):
+            os.makedirs(dir_pasta, exist_ok=True)
+        shutil.move(arquivo, dir_pasta)
+        print(nome_pasta)
+    except Exception as e:
+        print(f"Erro: {e}")
